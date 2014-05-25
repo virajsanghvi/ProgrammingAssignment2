2d9ecb18915394c34d2ac8be10f633c8d1177b43
Viraj Sanghvi <vsanghvi@gmail.com>
2014-05-24 22:55:17 -0700

    Initial comimt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This collection of functions creates a CacheMatrix
+## that has cacheable operations. Currently, caching the result
+## of 'solve' is supported.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a list containing methods that 
+## give you the ability to generalize writing cache-enabled
+## methods.
+##
+## Input: 
+##   x: a matrix (optional)
+## Output:
+##   list(
+##     set(y): set the current matrix
+##     get(): returns the current matrix
+##     setsolve(solve): set the solve value for the current matrix
+##     getsolve(): get the solve value for the current matrix
+##  )
 makeCacheMatrix <- function(x = matrix()) {
-
+  # initialize value of solve to NULL
+  s <- NULL
+  
+  set <- function(y) {
+    # on setting a new matrix, reset value of solve
+    x <<- y
+    s <<- NULL
+  }
+  
+  get <- function() x
+  
+  setsolve <- function(solve) s <<- solve
+  
+  getsolve <- function() s
+  
+  # return list of methods
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve calculates the solve value for a CacheMatrix
+## Input:
+##   x: a CacheMatrix
+## Output:
+##   matrix: value of solve(x)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # attempt to get current value of solve if it exists
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  
+  # otherwise, calculate value and set it
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  
+  # return it
+  s
 }
